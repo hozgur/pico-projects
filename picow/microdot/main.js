c3ef3b1d5eba83f6f75aeb7090e2af4036445a51
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2022-11-10 20:00:36 +0300

    devam

--- picow/microdot/main.js
+++ picow/microdot/main.js
@@ -1,3 +1,9 @@
+let rpm = 100;
+const mmperrev = 10;
+
+let mmIncrease = 1;
+let run = false;
+
 window.addEventListener('load', function() {
   console.log('window loaded');
 var content = document.getElementById('content');
@@ -10,13 +16,56 @@ var content = document.getElementById('content');
           document.getElementById("command").addEventListener("keypress", onCommandKeyPress);
       }
   };
-  xhr.send(null);
-  
+  xhr.send(null);  
 });
 
-let onClickStart = () => {
-    console.log("start");
+let sendCommand = (command) => {
+  console.log('sending command: ' + command);
+  $.ajax({
+    url: "/command",
+    type: "POST",
+    data: {command: command},
+    success: function(data) {
+      console.log(data);
+    }
+  });
 }
+
+let onClickStart = () => {
+  if (run == false) {
+    run = true;
+    turn();
+    console.log("start");
+    setTimeout(turn, timeout);
+  }
+}
+
+let onClickStop = () => {
+  run = false;
+  console.log("stop");
+}
+
+let onRPMChange = () => {
+  rpm = $("#speed").val();
+  $("#rpm").text(rpm);
+}
+
+let turn = () => {
+  if (run == true) {
+    let timeout = 300;
+    if (rpm > 0) {
+      let mmpermin = rpm * mmperrev ;
+      timeout = mmIncrease * 60000 / mmpermin;
+      if (timeout < 200) {
+        timeout = 200;
+        mmIncrease = timeout * mmpermin / 60000;
+      }
+      sendCommand(`G1 G91 X${mmIncrease} F${mmpermin}`);
+    }
+   setTimeout(turn, timeout-1);
+  }
+}
+
 
 let onCommandKeyPress = (e) => {
   console.log(e);
@@ -27,13 +76,5 @@ let onCommandKeyPress = (e) => {
 }
 
 let onSendCommand = () => {
-  console.log($("#command").val());
-  $.ajax({
-    url: "/command",
-    type: "POST",
-    data: {command: $("#command").val()},
-    success: function(data) {
-      console.log(data);
-    }
-  });
+  sendCommand($("#command").val());
 }
