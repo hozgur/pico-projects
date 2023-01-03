5f62e850ac07628f9468319dcb3ce0a3fc070e31
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2023-01-03 10:22:00 +0300

    ble test

--- dcmotor/main.py
+++ dcmotor/main.py
@@ -1,11 +1,11 @@
 
-from machine import Pin, PWM
+from machine import Pin, PWM, ADC
 import time
 import rp2
+from micropython import const
 led = Pin(25,Pin.OUT)
 led.on()
-
-
+pot = ADC(26)
 
 @rp2.asm_pio()
 def pulse_counter():
@@ -36,32 +36,40 @@ class PulseCounter:
 
 encoder = PulseCounter(0,Pin(17,Pin.IN,Pin.PULL_UP))
 pwm = PWM(Pin(12))
-# Set the PWM frequency.
+
+ppr = const(2000) # Pulse per Rotation (Encoder)
+
 pwm.freq(1000)
 pwm_duty = 20000
 pwm.duty_u16(pwm_duty)
 
-target_pps = 10
+target_rpm = 1200
 
 start_time = time.ticks_ms() # get millisecond counter
 start_encoder = encoder.get_pulse_count()
-eprev = 0
-eintegral = 0
 
-while True:
-    #time.sleep(0.1)    
+duty_offset = 22000
+cur_offset = duty_offset
+
+eprev = 0 # for PID derivative calculation
+eintegral = 0 # for PID integral calculation
+
+while True:    
     deltaT = time.ticks_diff(time.ticks_ms(), start_time) # compute time difference
     deltaE = start_encoder - encoder.get_pulse_count()
     start_time = time.ticks_ms() # get millisecond counter
     start_encoder = encoder.get_pulse_count()
     
-    pps = deltaE / deltaT
-    e = (target_pps - pps)
+    pps = 1000 * deltaE / deltaT # pulse per second
+    rpm = 60 * pps / ppr
+    
+    e = (target_rpm - rpm)
+    
     
     # PID
-    kp = 500
-    kd = 5
-    ki = 0.01
+    kp = 30
+    kd = 0
+    ki = 0.0001
     dedt = (e - eprev)/deltaT/1e-3
     eintegral = eintegral + e * deltaT/1e-3
     
@@ -69,12 +77,22 @@ while True:
     u = kp * e + kd * dedt + ki * eintegral
     
     eprev = e
-    pwm_duty = int(u) + 22000
+    iu = int(u)
+    
+    pwm_duty = iu + cur_offset
     if pwm_duty > 65535:
         pwm_duty = 65535
     if pwm_duty < 0:
         pwm_duty = 0
     pwm.duty_u16(pwm_duty)
-        
-    #print(u, pps, start_encoder)
-    #print(start_encoder)
+    adc_val = (pot.read_u16() -12000) >> 8
+    if adc_val < 0:
+        adc_val = 0
+    target_rpm = 8 * adc_val
+    if adc_val == 0:
+        cur_offset = 0
+    else:
+        cur_offset = duty_offset
+    #print(target_rpm,pwm_duty)
+    
+    
---
+++ esp32/esp-c3-13/esp_c3_13.py
@@ -0,0 +1,7 @@
+from machine import Pin
+
+redLed = Pin(5, Pin.OUT)
+greenLed = Pin(3, Pin.OUT)
+blueLed = Pin(4, Pin.OUT)
+led1 = Pin(19, Pin.OUT)
+led2 = Pin(18, Pin.OUT)
